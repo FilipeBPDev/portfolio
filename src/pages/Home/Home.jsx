d7a1d2a47176b90cffaa11f819bb83b096677c4b
Filipe <devfilipebp@gmail.com>
2025-06-10 13:10:45 -0300

    refinamento home -Fade in Hero -  Efeito escrita H1 - Scroll -Ícone seta botão

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,35 @@
+import Typewriter from 'typewriter-effect';
 import styles from './Home.module.css';
 
-
 export default function Home() {
-  return (
-    <main className={styles.container}>
-      {/* Hero Section */}
-      <section className={styles.hero}>
+
+return (
+<main className={styles.container}>
+    {/* Hero Section */}
+    <section className={styles.hero}>
         <div className={styles.heroBox}>
-          <h1>Olá, meu nome é Filipe Batista</h1>
-          <p>
-            Desenvolvedor web orientado a resultados:
-            <br />
-            <strong>
-              código eficiente + design intuitivo para produtos que entregam
-            </strong>
-          </p>
-          <a href="#sobre" className={styles.button}>Sobre mim</a>
+            <h1>
+              <Typewriter
+                options={{
+                  strings: ['Olá, meu nome é Filipe Batista', 'Dev web, apaixonado por tecnologia.'],
+                  autoStart: true,
+                  loop: true,
+                  cursor: '<span style="color: var(--color-primary)">|</span>'
+                }}
+              />
+            </h1>
+            <p>
+                Desenvolvedor orientado a resultados com 
+                <br />
+                <strong>
+                    código eficiente + design intuitivo para produtos que entregam
+                </strong>
+            </p>
+            <a href="#sobre" className={styles.button}>
+                Sobre mim <span className={styles.arrow}>↓</span>
+            </a>
         </div>
-      </section>
-    </main>
-  )
+    </section>
+</main>
+)
 }
